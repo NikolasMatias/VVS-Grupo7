4c940cd18ca90087921c578088c52cca7c9bac27
NikolasMatias <nikolas.matias500@gmail.com>
2016-06-18 22:11:32 -0300

    Parte do Nikolas da Criação dos Casos de Testes feita com sucesso.

--- AdmitiRH/test/recursosHumanos/negocio/CargoNegTest.java
+++ AdmitiRH/test/recursosHumanos/negocio/CargoNegTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import recursosHumanos.entidade.Candidato;
 import recursosHumanos.entidade.Cargo;
+import recursosHumanos.enumeradores.EnumTipoDocumentoHabilitacao;
 
 /**
  *
@@ -38,7 +39,223 @@ public class CargoNegTest {
     @After
     public void tearDown() {
     }
-
+    
+    /** Nikolas Matias
+     * Caso de teste 23 - Documento Igual a Null
+     */
+    @Test
+    public void CT23() throws Exception {
+        System.out.println("Documento Igual a Null");
+        Cargo cargo = null;
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDescricao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 24 - Documento não preenchido
+     */
+    @Test
+    public void CT24() throws Exception {
+        System.out.println("Documento não preenchido.");
+        EnumTipoDocumentoHabilitacao Documento;
+        Cargo cargo = new Cargo("Engenheiro",12000,Documento,50);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 25 - Documento Incorreto.
+     */
+    @Test
+    public void CT25() throws Exception {
+        System.out.println("Documento Incorreto.");
+        Cargo cargo = new Cargo("Médico",12000,EnumTipoDocumentoHabilitacao.CNH,10);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 26 - Documento Incorreto.
+     */
+    @Test
+    public void CT26() throws Exception {
+        System.out.println("Documento Incorreto.");
+        Cargo cargo = new Cargo("Médico",12000,EnumTipoDocumentoHabilitacao.CREA,10);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 27 - Documento Igual a null.
+     */
+    @Test
+    public void CT27() throws Exception {
+        System.out.println("Documento igual a null.");
+        EnumTipoDocumentoHabilitacao documento = null;
+        Cargo cargo = new Cargo("Médico",12000,documento,10);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 28 - Documento Incorreto.
+     */
+    @Test
+    public void CT28() throws Exception {
+        System.out.println("Documento não preenchido.");
+        EnumTipoDocumentoHabilitacao documento;
+        Cargo cargo = new Cargo("Médico",12000,documento,10);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 29 - Documento não necessário.
+     */
+    @Test
+    public void CT29() throws Exception {
+        System.out.println("Documento não necessário.");
+        Cargo cargo = new Cargo("Técnico em Informática",12000,EnumTipoDocumentoHabilitacao.CNH,20);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 30 - Documento não necessário..
+     */
+    @Test
+    public void CT30() throws Exception {
+        System.out.println("Documento não necessário.");
+        Cargo cargo = new Cargo("Técnico em Informática",12000,EnumTipoDocumentoHabilitacao.CREA,20);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 31 - Documento igual a null.
+     */
+    @Test
+    public void CT31() throws Exception {
+        System.out.println("Documento igual a null.");
+        EnumTipoDocumentoHabilitacao documento = null;
+        Cargo cargo = new Cargo("Técnico em Informática",12000,documento,20);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /** Nikolas Matias
+     * Caso de teste 32 - Documento não necessário..
+     */
+    @Test
+    public void CT32() throws Exception {
+        System.out.println("Documento não necessário.");
+        Cargo cargo = new Cargo("Técnico em Informática",12000,EnumTipoDocumentoHabilitacao.CRM,20);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+     /** Nikolas Matias
+     * Caso de teste 33 - Documento não Preenchido.
+     */
+    @Test
+    public void CT33() throws Exception {
+        System.out.println("Documento não Preenchido.");
+        EnumTipoDocumentoHabilitacao documento;
+        Cargo cargo = new Cargo("Motorista",12000,documento,15);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+     /** Nikolas Matias
+     * Caso de teste 34 - Documento Incorreto.
+     */
+    @Test
+    public void CT34() throws Exception {
+        System.out.println("Documento Incorreto.");
+        Cargo cargo = new Cargo("Motorista",12000,EnumTipoDocumentoHabilitacao.CREA,15);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+     /** Nikolas Matias
+     * Caso de teste 35 - Documento não necessário..
+     */
+    @Test
+    public void CT35() throws Exception {
+        System.out.println("Documento Igual a null.");
+        EnumTipoDocumentoHabilitacao documento = null;
+        Cargo cargo = new Cargo("Motorista",12000,documento,15);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+     /** Nikolas Matias
+     * Caso de teste 36 - Documento não necessário..
+     */
+    @Test
+    public void CT36() throws Exception {
+        System.out.println("Documento Incorreto.");
+        Cargo cargo = new Cargo("Motorista",12000,EnumTipoDocumentoHabilitacao.CRM,15);
+        CargoNeg instance = new CargoNeg();
+        boolean expResult = false;
+        boolean result = instance.validaDocumentoHabilitacao(cargo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
     /**
      * Test of validaDescricao method, of class CargoNeg.
      */
